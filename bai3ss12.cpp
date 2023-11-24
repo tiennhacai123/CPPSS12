7b8498e0f483c54bf515454668c99861c5224b26
Tiến LD <147360077+tiennhacai123@users.noreply.github.com>
2023-11-24 08:01:22 +0700

    Add files via upload

---
+++ bai3ss12.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+int is_prime(int number) {
+    if (number < 2) {
+        return 0; // False
+    }
+    for (int i = 2; i * i <= number; i++) {
+        if (number % i == 0) {
+            return 0; // False
+        }
+    }
+    return 1; // True
+}
+int count_primes_in_array(int arr[], int size) {
+    int count = 0;
+    for (int i = 0; i < size; i++) {
+        if (is_prime(arr[i])) {
+            count++;
+        }
+    }
+    return count;
+}
+
+int main() {
+    int size1, size2;
+
+    printf("Nhap kich thuoc cua mang 1: ");
+    scanf("%d", &size1);
+
+    printf("Nhap kich thuoc cua mang 2: ");
+    scanf("%d", &size2);
+    int arr1[size1], arr2[size2];
+
+    printf("Nhap cac phan tu cua mang 1:\n");
+    for (int i = 0; i < size1; i++) {
+        scanf("%d", &arr1[i]);
+    }
+
+    printf("Nhap cac phan tu cua mang 2:\n");
+    for (int i = 0; i < size2; i++) {
+        scanf("%d", &arr2[i]);
+    }
+    int count1 = count_primes_in_array(arr1, size1);
+    int count2 = count_primes_in_array(arr2, size2);
+    printf("So luong so nguyen to trong mang 1: %d\n", count1);
+    printf("So luong so nguyen to trong mang 2: %d\n", count2);
+    if (count1 > count2) {
+        printf("Mang 1 co nhieu so nguyen to hon.\n");
+    } else if (count1 < count2) {
+        printf("Mang 2 co nhieu so nguyen to hon.\n");
+    } else {
+        printf("Hai mang co so luong so nguyen to bang nhau.\n");
+    }
+
+    return 0;
+}
+
